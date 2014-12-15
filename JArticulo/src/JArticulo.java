d11003fdfba8cb3146831be7fa1200e4dbbfb867
alejandro93 <alejandrobp93@gmail.com>
2014-12-15 08:28:54 +0100

    Actualizado y terminado JArticulo 15/12/2014

--- JArticulo/src/JArticulo.java
+++ JArticulo/src/JArticulo.java
@@ -13,93 +13,99 @@ public class JArticulo {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		/*Menu*/
-		System.out.println("Menu\n----------------");
-		System.out.println("0 = Salir");
-		System.out.println("1 = Nuevo");
-		System.out.println("2 = Editar");
-		System.out.println("3 = Eliminar");
-		System.out.println("4 = Visualizar");
-		
-		final int eleccion = scanner.nextInt();
-		
-			switch(eleccion){
-			case 0: {
-				System.out.println("Éxito al salir");
-				return;
-				}
-			case 1: {
-				System.out.println("Introduzca los datos del nuevo elemento");
-				int id = scanner.nextInt();
-				String nombre = scanner.nextLine();
-				int precio = scanner.nextInt();
-				String cat = scanner.nextLine();
-				anyadir(id, nombre, precio, cat);
-				break;
-			}
-			case 2: {
-				visual();
-				System.out.println("Escribe el id del elemento que desea modificar");
-				int id = scanner.nextInt();
-				System.out.println("Escribe el nuevo nombre del elemento");
-				String nombre = scanner.nextLine();
-				System.out.println("Escribe el nuevo precio del elemento");
-				int precio = scanner.nextInt();
-				System.out.println("Escribe la nueva categoria del elemento");
-				String cat = scanner.nextLine();
-				editar(id, nombre, precio, cat);
-				break;
-			}
-			case 3: {
-				visual();
-				System.out.println("Escribe el id del elemento que quiere eliminar: ");
-				int id = scanner.nextInt();
-				eliminar(id);
-				System.out.println("Elemento eliminado con éxito");
-				break;
-			}
-			case 4: {
-				System.out.println("Listado:\n******************************************");
-				visualizar();
-				System.out.println("*********************************************");
-				break;
-			}
-		
-			
-		}
-	
+		int eleccion;
 
+		while(true){
+			/*Menu*/
+			System.out.println("\nMenu\n----------------");
+			System.out.println("0 = Salir");
+			System.out.println("1 = Nuevo");
+			System.out.println("2 = Editar");
+			System.out.println("3 = Eliminar");
+			System.out.println("4 = Visualizar");
+			
+			eleccion = scanner.nextInt();
+			
+			switch(eleccion){
+				case 0: {  //Salir
+					System.out.println("Éxito al salir");
+					return;
+					}
+				case 1: {  //Añadir
+					System.out.println("Introduce nombre:");
+					String nombre = scanner.next();
+					System.out.println("Introduce precio:");
+					int precio = scanner.nextInt();
+					scanner.nextLine();
+					System.out.println("Introduce categoría:");
+					String cat = scanner.nextLine();
+					anyadir(nombre, precio, cat);
+					System.out.println("Artículo añadido con éxito");
+					break;
+				}
+				case 2: {  //Editar
+					visual();
+					System.out.println("Escribe el id del elemento que desea modificar");
+					int id = scanner.nextInt();
+					scanner.nextLine();
+					System.out.println("Escribe el nuevo nombre del elemento");
+					String nombre = scanner.nextLine();
+					System.out.println("Escribe el nuevo precio del elemento");
+					int precio = scanner.nextInt();
+					scanner.nextLine();
+					System.out.println("Escribe la nueva categoria del elemento");
+					String cat = scanner.nextLine();
+					System.out.println();
+					editar(id, nombre, precio, cat);
+					System.out.println("\nModificado con éxito\n");
+					break;
+				}
+				case 3: {  //Eliminar
+					visual();
+					System.out.println("Escribe el id del elemento que quiere eliminar: ");
+					int id = scanner.nextInt();
+					eliminar(id);
+					System.out.println("Eliminado con éxito");
+					break;
+				}
+				case 4: {  //Visualizar
+					System.out.println("Listado:\n*****************************************************************************");
+					visualizar();
+					System.out.println("*********************************************************************************");
+					break;
+				}
+			}
+		}
 	}
+		
 	
-	public static void anyadir(int id, String nombre, int precio, String cat) throws SQLException{
+	public static void anyadir(String nombre, int precio, String cat) throws SQLException{
 		connection = connection();
 		PreparedStatement insert = connection.prepareStatement(
-				"insert into categoria (id, nombre, precio, cat) values (?, ?, ?, ?)"
+				"insert into categoria (nombre, precio, cat) values (?, ?, ?)"
 				);
-		insert.setObject(1, id);
-		insert.setObject(2, nombre);
-		insert.setObject(3, precio);
-		insert.setObject(4, cat);
-		resultSet = insert.executeQuery();
+		insert.setObject(1, nombre);
+		insert.setObject(2, precio);
+		insert.setObject(3, cat);
+		insert.execute();
 		insert.close();
-		resultSet.close();
 		connection.close();
 	}
 	public static void editar(int id, String nombre, int precio, String cat) throws SQLException{
 		connection = connection();
 		PreparedStatement update = connection.prepareStatement(
-				"update categoria set id=?, nombre=?, precio=?, cat=? "
+				"update categoria set nombre=?, precio=?, cat=? where id=?"
 				);
-		update.setObject(1, id);
-		update.setObject(2, nombre);
-		update.setObject(3, precio);
-		update.setObject(4, cat);
-		resultSet = update.executeQuery();
+		update.setObject(1, nombre);
+		update.setObject(2, precio);
+		update.setObject(3, cat);
+		update.setObject(4, id);
+		update.executeUpdate();
 		PreparedStatement listar = connection.prepareStatement("select * from categoria where id=?");
 		listar.setObject(1, id);
 		resultSet = listar.executeQuery();
 		while(resultSet.next()){
-			System.out.printf("id=%4s    nombre=%s\n", resultSet.getObject("id"), resultSet.getObject("nombre"));
+			System.out.printf("id=%d nombre=%s precio=%d categoria=%s\n", resultSet.getObject("id"), resultSet.getObject("nombre"), resultSet.getObject("precio"), resultSet.getObject("cat"));
 		}
 		listar.close();
 		update.close();
@@ -111,10 +117,9 @@ public class JArticulo {
 		PreparedStatement delete = connection.prepareStatement(
 				"delete from categoria where id=?"
 				);
-		delete.setObject(1, id);
-		resultSet = delete.executeQuery();
+		delete.setInt(1, id);
+		delete.execute();
 		delete.close();
-		resultSet.close();
 		connection.close();
 	}
 	public static void visual() throws SQLException{
@@ -122,7 +127,7 @@ public class JArticulo {
 		Statement listar = connection.createStatement();
 		resultSet = listar.executeQuery("select * from categoria");
 		while(resultSet.next()){
-			System.out.printf("id=%4s    nombre=%s\n", resultSet.getObject("id"), resultSet.getObject("nombre"));
+			System.out.printf("id=%4d    nombre=%4s\n", resultSet.getObject("id"), resultSet.getObject("nombre"));
 		}
 		listar.close();
 		resultSet.close();
@@ -134,7 +139,7 @@ public class JArticulo {
 		Statement listar = connection.createStatement();
 		resultSet = listar.executeQuery("select * from categoria");
 		while(resultSet.next()){
-			System.out.printf("id=%4s    nombre=%s    precio=%s    categoria=%s\n", resultSet.getObject("id"), resultSet.getObject("nombre"), resultSet.getObject("precio"), resultSet.getObject("cat"));
+			System.out.printf("|id=%4d nombre=  %4s   precio=%4d   categoria=  %4s|\n", resultSet.getObject("id"), resultSet.getObject("nombre"), resultSet.getObject("precio"), resultSet.getObject("cat"));
 		}
 		listar.close();
 		connection.close();
